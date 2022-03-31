bc740f345a9a5072ded756d3c11ea9f3eee58e87
Roselinde <31527188+Roselinde@users.noreply.github.com>
2022-03-31 09:23:41 +0700

    Initial push
    1.0

---
+++ BetterBagsBackpacksforPLLandSLEO/media/lua/client/PawLowLoot_Tweaks.lua
@@ -0,0 +1,80 @@
+require("ItemTweaker_Core");
+
+
+if getActivatedMods():contains("PLLoot") then 
+
+-- Adventurer's Bag
+TweakItem("Base.smallback","WeightReduction","80");
+TweakItem("Base.smallback","Weight","0.5");
+TweakItem("Base.smallback","Capacity","20");
+TweakItem("Base.smallback","RunSpeedModifier","0.99");
+
+-- Large Tactical Waist Bag (Back)
+TweakItem("Base.TacticalWaistBagBack","WeightReduction","95");
+TweakItem("Base.TacticalWaistBagBack","Weight","0.2");
+TweakItem("Base.TacticalWaistBagBack","Capacity","5");
+TweakItem("Base.TacticalWaistBagBack","RunSpeedModifier","0.99");
+
+-- Large Tactical Waist Bag (Front)
+TweakItem("Base.TacticalWaistBagFront","WeightReduction","95");
+TweakItem("Base.TacticalWaistBagFront","Weight","0.2");
+TweakItem("Base.TacticalWaistBagFront","Capacity","5");
+TweakItem("Base.TacticalWaistBagFront","RunSpeedModifier","0.99");
+
+-- Large Medical Waist Bag (Back)
+TweakItem("Base.TacticalWaistBagBackMed","WeightReduction","95");
+TweakItem("Base.TacticalWaistBagBackMed","Weight","0.2");
+TweakItem("Base.TacticalWaistBagBackMed","Capacity","5");
+TweakItem("Base.TacticalWaistBagBackMed","RunSpeedModifier","0.99");
+
+-- Large Medical Waist Bag (Front)
+TweakItem("Base.TacticalWaistBagFrontMed","WeightReduction","95");
+TweakItem("Base.TacticalWaistBagFrontMed","Weight","0.2");
+TweakItem("Base.TacticalWaistBagFrontMed","Capacity","5");
+TweakItem("Base.TacticalWaistBagFrontMed","RunSpeedModifier","0.99");
+
+-- Rogue Waist Bag
+TweakItem("Base.RogueWaistBag","WeightReduction","95");
+TweakItem("Base.RogueWaistBag","Weight","0.2");
+TweakItem("Base.RogueWaistBag","Capacity","5");
+TweakItem("Base.RogueWaistBag","RunSpeedModifier","0.99");
+
+-- Medical Waist Pouches
+TweakItem("Base.medbag","WeightReduction","95");
+TweakItem("Base.medbag","Weight","0.1");
+TweakItem("Base.medbag","Capacity","4");
+TweakItem("Base.medbag","RunSpeedModifier","0.99");
+
+-- Waist Pouches
+TweakItem("Base.medbag2","WeightReduction","95");
+TweakItem("Base.medbag2","Weight","0.1");
+TweakItem("Base.medbag2","Capacity","4");
+TweakItem("Base.medbag2","RunSpeedModifier","0.99");
+
+-- Belt With Front Pockets
+TweakItem("Base.BeltRig","WeightReduction","95");
+TweakItem("Base.BeltRig","Weight","0.2");
+TweakItem("Base.BeltRig","Capacity","5");
+TweakItem("Base.BeltRig","RunSpeedModifier","0.99");
+
+-- First Aid Backpack
+TweakItem("Base.Bag_ParaMedic","WeightReduction","80");
+TweakItem("Base.Bag_ParaMedic","Weight","0.5");
+TweakItem("Base.Bag_Schoolbag","Capacity","20");
+TweakItem("Base.Bag_ParaMedic","RunSpeedModifier","0.99");
+
+-- Carrier Vest (Tight)
+TweakItem("Base.MVest","WeightReduction","95");
+TweakItem("Base.MVest","Weight","0.5");
+TweakItem("Base.MVest","Capacity","5");
+TweakItem("Base.MVest","RunSpeedModifier","0.99");
+
+-- Carrier Vest (Loose)
+TweakItem("Base.MVest2","WeightReduction","95");
+TweakItem("Base.MVest2","Weight","0.5");
+TweakItem("Base.MVest2","Capacity","5");
+TweakItem("Base.MVest2","RunSpeedModifier","0.99");
+
+end
+
+
---
+++ BetterBagsBackpacksforPLLandSLEO/media/lua/client/SharksLawEnforcementOverhaul_Tweaks.lua
@@ -0,0 +1,59 @@
+require("ItemTweaker_Core");
+
+
+if getActivatedMods():contains("SLEO") then 
+
+-- Police Duffel Bag
+TweakItem("SLEOClothing.Bag_DuffelPolice","WeightReduction","80");
+TweakItem("SLEOClothing.Bag_DuffelPolice","Weight","0.5");
+TweakItem("SLEOClothing.Bag_DuffelPolice","Capacity","23");
+TweakItem("SLEOClothing.Bag_DuffelPolice","RunSpeedModifier","0.99");
+
+-- SLEOClothing.Bag_DuffelSheriff
+TweakItem("SLEOClothing.Bag_DuffelSheriff","WeightReduction","80");
+TweakItem("SLEOClothing.Bag_DuffelSheriff","Weight","0.5");
+TweakItem("SLEOClothing.Bag_DuffelSheriff","Capacity","23");
+TweakItem("SLEOClothing.Bag_DuffelSheriff","RunSpeedModifier","0.99");
+
+-- Black Utility Bag
+TweakItem("SLEOClothing.Bag_PoliceUtilityBag","WeightReduction","70");
+TweakItem("SLEOClothing.Bag_PoliceUtilityBag","Weight","0.5");
+TweakItem("SLEOClothing.Bag_PoliceUtilityBag","Capacity","20");
+TweakItem("SLEOClothing.Bag_PoliceUtilityBag","RunSpeedModifier","0.99");
+
+-- Green Utility Bag
+TweakItem("SLEOClothing.Bag_PoliceUtilityBagGreen","WeightReduction","70");
+TweakItem("SLEOClothing.Bag_PoliceUtilityBagGreen","Weight","0.5");
+TweakItem("SLEOClothing.Bag_PoliceUtilityBagGreen","Capacity","20");
+TweakItem("SLEOClothing.Bag_PoliceUtilityBagGreen","RunSpeedModifier","0.99");
+
+-- Black Leg Pouch (Right)
+TweakItem("SLEOClothing.BlackLegPouch_RLeg","WeightReduction","95");
+TweakItem("SLEOClothing.BlackLegPouch_RLeg","Weight","0.2");
+TweakItem("SLEOClothing.BlackLegPouch_RLeg","Capacity","5");
+TweakItem("SLEOClothing.BlackLegPouch_RLeg","RunSpeedModifier","0.99");
+
+-- Black Leg Pouch (Left)
+TweakItem("SLEOClothing.BlackLegPouch_LLeg","WeightReduction","95");
+TweakItem("SLEOClothing.BlackLegPouch_LLeg","Weight","0.2");
+TweakItem("SLEOClothing.BlackLegPouch_LLeg","Capacity","5");
+TweakItem("SLEOClothing.BlackLegPouch_LLeg","RunSpeedModifier","0.99");
+
+-- Green Leg Pouch (Right)
+TweakItem("SLEOClothing.GreenLegPouch_RLeg","WeightReduction","95");
+TweakItem("SLEOClothing.GreenLegPouch_RLeg","Weight","0.2");
+TweakItem("SLEOClothing.GreenLegPouch_RLeg","Capacity","5");
+TweakItem("SLEOClothing.GreenLegPouch_RLeg","RunSpeedModifier","0.99");
+
+-- Green Leg Pouch (Left)
+TweakItem("SLEOClothing.GreenLegPouch_LLeg","WeightReduction","95");
+TweakItem("SLEOClothing.GreenLegPouch_LLeg","Weight","0.2");
+TweakItem("SLEOClothing.GreenLegPouch_LLeg","Capacity","5");
+TweakItem("SLEOClothing.GreenLegPouch_LLeg","RunSpeedModifier","0.99");
+
+
+
+end
+
+
+
